3a924d699a75766fc285751e1e11ad05eb24a626
Jonathan Koss <jonathan_koss@yahoo.com>
2019-07-20 21:01:22 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -25,3 +25,41 @@ def list(songs)
   end 
 end 
 
+def play(songs)
+  puts "Please enter a song name or number:"
+  inp = gets.chomp 
+  num = inp.to_i - 1
+  
+  if songs.include?(inp)
+    index_of_songs = songs.index(inp)
+    puts "Playing #{songs[index_of_songs]}"
+  elsif num >= 0 && !!songs[num]
+    puts "Playing #{songs[num]}"
+  else 
+    puts "Invalid input, please try again"
+  end 
+end 
+
+def exit_jukebox
+  puts "Goodbye"
+end 
+
+def run(songs)
+  help 
+  while true 
+    puts "Please enter a command:"
+    inp = gets.strip 
+    
+    if inp == "exit"
+      exit_jukebox
+      break
+    elsif inp == "list"
+      list(songs)
+    elsif inp == "play"
+      play(songs)
+    else 
+      puts "Invalid input"
+    end 
+  end 
+end 
+
